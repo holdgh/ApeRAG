c89928c4058f0bdee79d086e2ebc026b044d2d90
1aal <alal@zju.edu.cn>
2023-06-26 14:53:56 +0800

    test openAI

--- services/text2SQL/PostgreSQL.py
+++ services/text2SQL/PostgreSQL.py
@@ -5,13 +5,14 @@ from typing import Optional, Required
 
 from langchain.base_language import BaseLanguageModel
 from langchain.chat_models import ChatOpenAI
+from langchain.llms.base import LLM, BaseLLM
 from llama_index.prompts.default_prompts import DEFAULT_TEXT_TO_SQL_TMPL
 from llama_index.prompts.prompt_type import PromptType
 from services.text2SQL.base import SQLDataBase
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.llms import GPT4All, OpenAI
 from llama_index import SQLStructStoreIndex, ServiceContext, Prompt, \
-    LangchainEmbedding
+    LangchainEmbedding, LLMPredictor
 from llama_index.indices.struct_store import SQLContextContainerBuilder
 from sqlalchemy import create_engine
 
@@ -29,7 +30,7 @@ class PostgreSQL:
     def __init__(self, user, pw,
                  database_name: str,
                  host: Optional[str] = "localhost", port: Optional[str] = "5432",
-                 llm: Optional[BaseLanguageModel] = None,
+                 llm: Optional[BaseLLM] = None,
                  embed_model: Optional[LangchainEmbedding] = None,
                  prompt_text: Optional[str] = DEFAULT_TEXT_TO_SQL_TMPL):
         self.dialect = "postgresql"
@@ -43,11 +44,11 @@ class PostgreSQL:
         self.prompt_text = prompt_text
         self.db = None
         self.embed_model = None
-        if llm == None:
+        if llm is None:
             self.llm = GPT4All(model=local_llm_path, n_ctx=512)
         else:
             self.llm = llm
-        if embed_model != None:
+        if embed_model is None:
             self.embed_model = embed_model
 
     def _self_check(self):
@@ -77,29 +78,51 @@ class PostgreSQL:
 
     def query(self, query_str: str, sample_rows: Optional[int] = 3) -> str:
         self._connect()
-        promt = Prompt(
+        prompt = Prompt(
             self.prompt_text,
             stop_token="\nSQLResult:",
             prompt_type=PromptType.TEXT_TO_SQL,
         )
+        # llm_predictor = LLMPredictor(llm=llm)
+        # response_str, _ = llm_predictor.predict(prompt,
+        #                                         query_str=query_str,
+        #                                         schema=self.db.generate_sql_context(sample_rows),
+        #                                         dialect=self.dialect)
 
-        sc = ServiceContext.from_defaults(llm=self.llm, embed_model=self.embed_model)
-        context_builder = SQLContextContainerBuilder(sql_database=self.db,
-                                                     context_str=self.db.generate_sql_context(sample_rows))
+        if isinstance(self.llm, LLM):
+            # for local LLM
+            sc = ServiceContext.from_defaults(llm=self.llm, embed_model=self.embed_model)
 
-        # index
-        index = SQLStructStoreIndex(
-            service_context=sc,
-            sql_database=self.db,
-            sql_context_container=context_builder.build_context_container()
-        )
+            context_builder = SQLContextContainerBuilder(sql_database=self.db,
+                                                         context_str=self.db.generate_sql_context(sample_rows))
 
-        query_engine = index.as_query_engine(query_mode="nl", text_to_sql_prompt=promt)
-        response = query_engine.query(query_str)
-        return response.extra_info["sql_query"]
+            # index
+            index = SQLStructStoreIndex(
+                service_context=sc,
+                sql_database=self.db,
+                sql_context_container=context_builder.build_context_container()
+            )
+
+            query_engine = index.as_query_engine(query_mode="nl", text_to_sql_prompt=prompt)
+            response = query_engine.query(query_str)
+            return response.extra_info["sql_query"]
+        else:
+            # sc = ServiceContext.from_defaults(llm=self.llm, embed_model=self.embed_model)
+            # context_builder = SQLContextContainerBuilder(sql_database=self.db,
+            #                                              context_str=self.db.generate_sql_context(sample_rows))
+            # # index
+            index = SQLStructStoreIndex(
+                [],
+                sql_database=self.db,
+            )
+            query_engine = index.as_query_engine(query_mode="nl", text_to_sql_prompt=prompt)
+            response = query_engine.query(query_str)
+            return response.extra_info["sql_query"]
 
 
 if __name__ == '__main__':
-    pg = PostgreSQL(user="postgres", pw="", database_name="postgres")
+    llm = OpenAI()
+    # llmLocal = GPT4All(model=local_llm_path)
+    pg = PostgreSQL(user="postgres", pw="", database_name="postgres", llm=llm)
     response = pg.query("how old is Mary?")
     # print(response, len(response))
