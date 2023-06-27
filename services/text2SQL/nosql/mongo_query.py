fbed9ed347491d57b684ef2065bfc927ab22fd79
Meng YiPeng <413183498@qq.com>
2023-06-27 18:10:24 +0800

    add a optional way to use ssl connecting mongo

--- services/text2SQL/nosql/mongo_query.py
+++ services/text2SQL/nosql/mongo_query.py
@@ -4,23 +4,23 @@ from services.text2SQL.nosql.base import Nosql
 from llama_index.prompts.base import Prompt
 
 _MONGODB_PROMPT_TPL = (
-            "You are now an expert on mongodb，Given an input question, first create a syntactically correct MONGODB "
-            "query to run, then look at the results of the query and return the answer."
-            "You can order the results by a relevant column to return the most "
-            "interesting examples in the database.\n"
-            "Pay attention to use only the keys that you can see in the schema "
-            "description. "
-            "Be careful to not query for keys that do not exist. "
-            "Use the following format:\n"
-            "Question: Question here\n"
-            "Query: Query to run\n"
-            "Result: Result of the Query\n"
-            "Answer: Final answer here\n"
-            "Only use the keys with its type listed below.\n"
-            "{schema}\n"
-            "Question: {query_str}\n"
-            "Query: "
-        )
+    "You are now an expert on mongodb，Given an input question, first create a syntactically correct MONGODB "
+    "query to run, then look at the results of the query and return the answer."
+    "You can order the results by a relevant column to return the most "
+    "interesting examples in the database.\n"
+    "Pay attention to use only the keys that you can see in the schema "
+    "description. "
+    "Be careful to not query for keys that do not exist. "
+    "Use the following format:\n"
+    "Question: Question here\n"
+    "Query: Query to run\n"
+    "Result: Result of the Query\n"
+    "Answer: Final answer here\n"
+    "Only use the keys with its type listed below.\n"
+    "{schema}\n"
+    "Question: {query_str}\n"
+    "Query: "
+)
 
 _DEFAULT_PROMPT = Prompt(
     _MONGODB_PROMPT_TPL,
@@ -31,20 +31,30 @@ _DEFAULT_PROMPT = Prompt(
 
 class Mongo(Nosql):
     def __init__(
-        self,
-        host,
-        port,
-        collection,
-        pwd: Optional[str] = None,
-        db: Optional[str] = '',
-        prompt: Optional[Prompt] = _DEFAULT_PROMPT
+            self,
+            host,
+            port,
+            collection,
+            pwd: Optional[str] = None,
+            db: Optional[str] = '',
+            prompt: Optional[Prompt] = _DEFAULT_PROMPT
     ):
         super().__init__(host, port, pwd, prompt)
         self.db = db
         self.collection = collection
 
-    def connect(self):
-        self.conn = MongoClient("mongodb://"+self.host+':'+self.port)
+    def connect(self, ssl_enable=False, ssl_ca_certs='', ssl_certfile='', ssl_keyfile=''):
+        if ssl_enable:
+            ssl_options = {
+                'ssl': True,
+                'ssl_ca_certs': ssl_ca_certs,  # CA证书路径
+                'ssl_certfile': ssl_certfile,  # 客户端SSL证书路径
+                'ssl_keyfile': ssl_keyfile,  # 客户端私钥路径
+                # 其他可选参数
+            }
+            self.conn = MongoClient("mongodb://" + self.host + ':' + self.port, **ssl_options)
+        else:
+            self.conn = MongoClient("mongodb://" + self.host + ':' + self.port)
 
     def text_to_query(self, text):
         # Connect to the MongoDB database and collection
@@ -73,5 +83,3 @@ class Mongo(Nosql):
     # pymongo does not support a way to directly execute js query statements
     def execute_query(self, query):
         return
-
-
