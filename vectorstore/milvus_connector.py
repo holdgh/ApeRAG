b2fe99a2543793bf0134e5ba25a82414e25c2c8c
nayutah <dullboy@apecloud.com>
2023-06-29 10:45:33 +0800

    support search abstract method

--- vectorstore/milvus_connector.py
+++ vectorstore/milvus_connector.py
@@ -20,15 +20,19 @@ class MilvusVectorStoreConnector(VectorStoreConnector):
         self.ctx = ctx
         self.collection_name = ctx.get("collection", "collection")
 
-        self.url = ctx.get("url", "http://localhost")
+        self.host = ctx.get("host", "localhost")
         self.port = ctx.get("port", 19530)
-        self.grpc_port = ctx.get("grpc_port", 6334)
-        self.prefer_grpc = ctx.get("prefer_grpc", False)
-        self.https = ctx.get("https", False)
-        self.timeout = ctx.get("timeout", 300)
-        self.vector_size = ctx.get("vector_size", 1536)
+        self.user = ctx.get("user", "")
+        self.password = ctx.get("password", "")
+        self.alias = f"{self.host}:{self.port}"
 
-        self.client = connections.connect("default", host="localhost", port="19530")
+        connections.connect(self.alias, host=self.host, port=self.port)
         print("connect successful")
+
+        # be careful that connections is a single instance in pymilvus
+        self.client = connections
         self.embedding = ctx.get("embedding", google.GoogleUnivSentEncoderEmbedding)
         self.store = MilvusVectorStore(client=self.client, collection_name=self.collection_name)
+
+    def search(self, **kwargs):
+        pass
