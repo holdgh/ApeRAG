8a680a56f9680cf1b1a618f97946d7b8f1fbaf15
Guo Ziang <ziang.gza@apecloud.com>
2023-06-30 10:21:51 +0800

    chore: return markdown

--- kubechat/chat/document_qa_consumer.py
+++ kubechat/chat/document_qa_consumer.py
@@ -110,7 +110,7 @@ class DocumentQAConsumer(WebsocketConsumer):
         self.send(text_data=self.stop_response())
 
         # save all tokens as a message to history
-        self.history.add_message(AIMessage(content=message, additional_kwargs={"role": "ai"}))
+        self.history.add_message(AIMessage(content=self.success_response(message), additional_kwargs={"role": "ai"}))
 
     @staticmethod
     def success_response(message, references=None):
@@ -141,6 +141,41 @@ class DocumentQAConsumer(WebsocketConsumer):
 
 
 class RandomConsumer(DocumentQAConsumer):
+    response = '''
+---
+title: KubeBlocks overview
+description: KubeBlocks, kbcli, multicloud
+keywords: [kubeblocks, overview, introduction]
+sidebar_position: 1
+---
+
+# KubeBlocks overview
+
+## Introduction
+
+KubeBlocks is an open-source, cloud-native data infrastructure designed to help application developers and platform engineers manage database and analytical workloads on Kubernetes. It is cloud-neutral and supports multiple cloud service providers, offering a unified and declarative approach to increase productivity in DevOps practices.
+
+The name KubeBlocks is derived from Kubernetes and LEGO blocks, which indicates that building database and analytical workloads on Kubernetes can be both productive and enjoyable, like playing with construction toys. KubeBlocks combines the large-scale production experiences of top cloud service providers with enhanced usability and stability.
+
+## Why you need KubeBlocks
+
+Kubernetes has become the de facto standard for container orchestration. It manages an ever-increasing number of stateless workloads with the scalability and availability provided by ReplicaSet and the rollout and rollback capabilities provided by Deployment. However, managing stateful workloads poses great challenges for Kubernetes. Although StatefulSet provides stable persistent storage and unique network identifiers, these abilities are far from enough for complex stateful workloads.
+
+To address these challenges, and solve the problem of complexity, KubeBlocks introduces ReplicationSet and ConsensusSet, with the following capabilities:
+
+- Role-based update order reduces downtime caused by upgrading versions, scaling, and rebooting.
+- Maintains the status of data replication and automatically repairs replication errors or delays.
+
+## Key features
+
+- Be compatible with AWS, GCP, Azure, and Alibaba Cloud.
+- Supports MySQL, PostgreSQL, Redis, MongoDB, Kafka, and more.
+- Provides production-level performance, resilience, scalability, and observability.
+- Simplifies day-2 operations, such as upgrading, scaling, monitoring, backup, and restore.
+- Contains a powerful and intuitive command line tool.
+- Sets up a full-stack, production-ready data infrastructure in minutes.
+'''
+
     def connect(self):
         collection_id, chat_id = extract_collection_and_chat_id(self.scope["path"])
         self.history = RedisChatMessageHistory(session_id=chat_id, url=settings.MEMORY_REDIS_URL)
@@ -151,7 +186,14 @@ class RandomConsumer(DocumentQAConsumer):
         print("disconnect: " + str(close_code))
 
     def predict(self, query):
-        for i in range(0, 20):
-            yield ''.join(random.choices(string.ascii_lowercase, k=random.randint(3, 10)))
-            # mock the thinking time
-            time.sleep(random.uniform(0.1, 1))
+        tokens = self.response.split(" ")
+        start = 0
+        length = len(tokens)
+        while start < len(tokens):
+            count = random.randint(5, 20)
+            end = start + count
+            if end > length:
+                end = length
+            yield " ".join(tokens[start:end])
+            start = end
+            time.sleep(random.uniform(0.1, 0.5))
