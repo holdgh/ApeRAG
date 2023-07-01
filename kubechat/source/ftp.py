841d1d8989ce4bab56227d49cbc7a4588a4e3f76
Meng YiPeng <413183498@qq.com>
2023-07-01 20:57:30 +0800

    add a document source of ftp and add the metadata from config

--- kubechat/source/ftp.py
+++ kubechat/source/ftp.py
@@ -32,21 +32,22 @@ def deal_the_path(ftp, collection, path='/'):
             ftp.cwd(file_path)  # Try to switch to the specified path (if it's a folder)
             deal_the_path(ftp, collection, file_path)  # Recursively process the subdirectory
         except error_perm:  # If it's not a folder, process the file
-            print(file_path)
-            temp_file = download_file(ftp, file_path)  # Download the file
-            temp_files.append(temp_file)
-            file_stat = os.stat(file_path)
-            document_instance = Document(
-                user=collection.user,
-                name=file_path,
-                status=DocumentStatus.PENDING,
-                size=file_stat.st_size,
-                collection=collection,
-                metadata=time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(file_stat.st_mtime))
-            )
-            document_instance.save()
+            if os.path.splitext(file)[1].lower() in DEFAULT_FILE_READER_CLS.keys():
+                print(file_path)
+                temp_file = download_file(ftp, file_path)  # Download the file
+                temp_files.append(temp_file)
+                file_stat = os.stat(file_path)
+                document_instance = Document(
+                    user=collection.user,
+                    name=file_path,
+                    status=DocumentStatus.PENDING,
+                    size=file_stat.st_size,
+                    collection=collection,
+                    metadata=time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(file_stat.st_mtime))
+                )
+                document_instance.save()
 
-            add_index_for_document.delay(document_instance.id)
+                add_index_for_document.delay(document_instance.id)
 
     for temp_file in temp_files:
         os.remove(temp_file)
