e41d828d88f61da02c1b50849380cee9488030a2
shiyu15 <m2457316417@outlook.com>
2023-07-02 02:27:40 +0800

    support s3 download and add index

--- kubechat/source/s3.py
+++ kubechat/source/s3.py
@@ -1,48 +1,60 @@
 import boto3
 import os
-import time
+from datetime import datetime
+import logging
 
 from kubechat.tasks.index import add_index_for_document
 from kubechat.models import Document, DocumentStatus, CollectionStatus
 from kubechat.tasks.local_directory_task import cron_collection_metadata
 from readers.Readers import DEFAULT_FILE_READER_CLS
 
-import logging
 logger = logging.getLogger(__name__)
 
 
-def connect_to_s3(access_key_id, access_key_secret, bucket_name, region):
-    s3 = boto3.resource('s3',
-                        aws_access_key_id=access_key_id,
-                        aws_secret_access_key=access_key_secret,
-                        region_name=region)
-    bucket = s3.Bucket(bucket_name)
-    return bucket
+def connect_to_s3(access_key_id, access_key_secret, region_name):
+    session = boto3.Session(aws_access_key_id=access_key_id,
+                            aws_secret_access_key=access_key_secret,
+                            region_name=region_name)
+    s3 = session.client("s3")
+    return s3
 
 
-def list_files(bucket):
-    for obj in bucket.objects.all():
-        print(obj.key)
+# download all s3 file in the bucket
+# in Kubechat/documents/s3/[your_bucket_name]
+# which is in .gitignore
+def download_file(s3, file_name, bucket_name):
+    current_path = os.path.dirname(os.path.abspath(__file__))
+    parent_path = os.path.dirname(os.path.dirname(current_path))
+    bucket_path = os.path.join(parent_path, "documents", "s3", bucket_name)
+    if not os.path.exists(bucket_path):
+        os.makedirs(bucket_path)
+    file_path = os.path.join(bucket_path, file_name)
+    s3.download_file(bucket_name, file_name, file_path)
+    return file_path
 
 
 def scanning_s3_add_index(bucket_name, access_key_id, access_key_secret, region, collection):
     collection.status = CollectionStatus.INACTIVE
     collection.save()
-    bucket = connect_to_s3(access_key_id, access_key_secret, bucket_name, region)
-
+    s3 = connect_to_s3(access_key_id, access_key_secret, region)
+    response = s3.list_objects_v2(
+        Bucket=bucket_name,
+    )
+    objs = response['Contents']
     try:
-        for obj in bucket.objects.all():
-            if os.path.splitext(obj.key)[1].lower() in DEFAULT_FILE_READER_CLS.keys():
+        for obj in objs:
+            if os.path.splitext(obj['Key'])[1].lower() in DEFAULT_FILE_READER_CLS.keys():
                 document_instance = Document(
                     user=collection.user,
-                    name=obj.key,
+                    name=obj['Key'],
                     status=DocumentStatus.PENDING,
-                    size=obj.size,
+                    size=obj['Size'],
                     collection=collection,
-                    metadata=time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(obj.last_modified))
+                    metadata=obj['LastModified'].strftime("%Y-%m-%d %H:%M:%S")
                 )
                 document_instance.save()
-                add_index_for_document.delay(document_instance.id)
+                file_path = download_file(s3, obj['Key'], bucket_name)
+                add_index_for_document.delay(document_instance.id, file_path)
     except Exception as e:
         logger.error(f"scanning_s3_add_index() error {e}")
 
