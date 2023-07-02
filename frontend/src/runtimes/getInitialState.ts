df2c8ef9c9ccd029e70d7bc313801d3c924ae579
申栋栋 <sailwebs@apecloud.com>
2023-07-02 17:38:44 +0800

    feat: avatar

--- frontend/src/runtimes/getInitialState.ts
+++ frontend/src/runtimes/getInitialState.ts
@@ -6,8 +6,18 @@ export type InitialStateType = {
 };
 
 export async function getInitialState(): Promise<InitialStateType> {
-  const user = await auth0.getIdTokenClaims();
+  let user = await auth0.getIdTokenClaims();
   if (!user) await auth0.loginWithRedirect();
+
+
+
+  user = {
+    ...user,
+    picture: 'https://img2.baidu.com/it/u=1582585185,2774298853&fm=253&fmt=auto&app=138&f=JPG?w=500&h=500',
+    nickname: 'ApeCloud'
+  }
+
+
 
   setUser(user);
 
