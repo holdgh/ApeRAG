de497c777bc5a162cffdd6cef4d4a0b8e70b0b26
kizuna-lek <asd98041@qq.com>
2023-07-02 12:31:48 +0800

    add redis test data

---
+++ services/text2SQL/nosql/test_data.py
@@ -0,0 +1,20 @@
+import redis
+
+
+def add_testdata_to_redis():
+    r = redis.Redis(host='localhost', port=6379, db=2)
+    pipe = r.pipeline()
+
+    pipe.set('foo', 'string')
+    pipe.set('size', 25)
+    pipe.lpush('fruits', 'apple', 'banana', 'orange')
+    pipe.sadd('hobbies', 'reading', 'swimming', 'coding')
+    pipe.hmset('person1', {'name': 'Alice', 'age': 30, 'city': 'New York'})
+    pipe.hmset('person2', {'name': 'Bob', 'age': 35, 'city': 'London'})
+    pipe.hmset('person3', {'name': 'Charlie', 'age': 40, 'city': 'Paris'})
+
+    pipe.execute()
+
+
+if __name__ == "__main__":
+    add_testdata_to_redis()
