25a7c16f0caa9b280820dc318401d97cfe85df07
Guo Ziang <ziang.gza@apecloud.com>
2023-07-04 13:29:38 +0800

    chore: support cors

--- config/settings.py
+++ config/settings.py
@@ -40,7 +40,7 @@ ALLOWED_HOSTS = ["*"]
 INSTALLED_APPS = [
     "daphne",
     "kubechat",
-    # 'kubechat.apps.KubechatConfig',
+    "corsheaders",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -135,6 +136,14 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# CORS
+CORS_ALLOWED_ORIGINS = [
+]
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    r"^https://\w+\.kubeblocks\.io$",
+    r"http://localhost:*",
+]
 
 # Auth0
 # Load Auth0 application settings into memory
