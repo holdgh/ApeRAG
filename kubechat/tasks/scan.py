ed9ef57cea702d793729970c2d20a27f6da0a1f3
Guo Ziang <ziang.gza@apecloud.com>
2023-07-10 10:52:41 +0800

    chore: fix ftp import error

--- kubechat/tasks/scan.py
+++ kubechat/tasks/scan.py
@@ -13,53 +13,54 @@ def scan_collection(collection_id):
 
     config = json.loads(collection.config)
 
-    if config["source"] == "system":
-        pass
-    elif config["source"] == "local":
-        from kubechat.source.local import scanning_dir_add_index
+    match config["source"]:
+        case "system":
+            pass
+        case "local":
+            from kubechat.source.local import scanning_dir_add_index
 
-        scanning_dir_add_index(config["path"], collection)
-    elif config["source"] == "s3":
-        from kubechat.source.s3 import scanning_s3_add_index
+            scanning_dir_add_index(config["path"], collection)
+        case "s3":
+            from kubechat.source.s3 import scanning_s3_add_index
 
-        scanning_s3_add_index(
-            config["bucket"],
-            config["access_key_id"],
-            config["secret_access_key"],
-            config["region"],
-            collection,
-        )
-    elif config["source"] == "oss":
-        from kubechat.source.oss import scanning_oss_add_index
+            scanning_s3_add_index(
+                config["bucket"],
+                config["access_key_id"],
+                config["secret_access_key"],
+                config["region"],
+                collection,
+            )
+        case "oss":
+            from kubechat.source.oss import scanning_oss_add_index
 
-        scanning_oss_add_index(
-            config["bucket"],
-            config["access_key_id"],
-            config["secret_access_key"],
-            config["region"],
-            config["dir"],
-            collection,
-        )
-    elif config["source"] == "ftp":
-        from kubechat.source.ftp import scanning_dir_add_index_from_ftp
+            scanning_oss_add_index(
+                config["bucket"],
+                config["access_key_id"],
+                config["secret_access_key"],
+                config["region"],
+                config["dir"],
+                collection,
+            )
+        case "ftp":
+            from kubechat.source.ftp import scanning_ftp_add_index
 
-        scanning_dir_add_index_from_ftp(
-            config["path"],
-            config["host"],
-            config["username"],
-            config["password"],
-            collection,
-        )
-    elif config["source"] == "email":
-        from kubechat.source.email import scanning_email_add_index
+            scanning_ftp_add_index(
+                config["path"],
+                config["host"],
+                config["username"],
+                config["password"],
+                collection,
+            )
+        case "email":
+            from kubechat.source.email import scanning_email_add_index
 
-        scanning_email_add_index(
-            config["pop_server"],
-            config["port"],
-            config["email_address"],
-            config["email_password"],
-            collection,
-        )
+            scanning_email_add_index(
+                config["pop_server"],
+                config["port"],
+                config["email_address"],
+                config["email_password"],
+                collection,
+            )
 
 
 @app.task(base=CustomLoadDocumentTask)
