c117adcd040b5c1f8026f33a482294afe586c90e
1aal <alal@zju.edu.cn>
2023-08-24 09:29:30 +0800

    add table parse for docx

--- readers/docx_reader_test.py
+++ readers/docx_reader_test.py
@@ -5,6 +5,6 @@ from docx_reader import MyDocxReader
 
 test_my_docx_reader = MyDocxReader()
 
-res = test_my_docx_reader.load_data(Path("/Users/alal/KubeChat/tests/reader_test/docx_reader/KubeBlocks overview.docx"))
+res = test_my_docx_reader.load_data(Path("/Users/alal/KubeChat/tests/reader_test/docx_reader/test-2.docx"))
 
 print(res)
