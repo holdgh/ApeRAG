f56e0da01d3aa3e6d07c4c3cfb0fb0b314fab7ab
Ziang Guo <ziang.gza@apecloud.com>
2023-10-17 11:59:19 +0800

    chore: support config (#273)

--- kubechat/views.py
+++ kubechat/views.py
@@ -702,6 +702,22 @@ async def delete_bot(request, bot_id):
     return success(bot.view())
 
 
+@api.get("/config")
+def config(request):
+    auth = {
+        "type": settings.AUTH_TYPE,
+    }
+
+    match settings.AUTH_TYPE:
+        case "auth0":
+            auth["auth_domain"] = settings.AUTH0_DOMAIN
+            auth["auth_app_id"] = settings.AUTH0_CLIENT_ID
+        case "authing":
+            auth["auth_domain"] = settings.AUTHING_DOMAIN
+            auth["auth_app_id"] = settings.AUTHING_APP_ID
+    return success({"auth": auth})
+
+
 def default_page(request, exception):
     return render(request, '404.html')
 
