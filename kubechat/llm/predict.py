a0f0239e4e5a4854735aff498343bb5983cfd780
Ziang Guo <ziang.gza@apecloud.com>
2023-11-01 14:30:21 +0800

    chore: support azure openai service (#352)

--- kubechat/llm/predict.py
+++ kubechat/llm/predict.py
@@ -61,11 +61,7 @@ class Predictor(ABC):
             case "chatgpt-4":
                 kwargs["model"] = "gpt-4"
                 return OpenAIPredictor(**kwargs)
-            case "azure-chatgpt-3.5":
-                kwargs["model"] = "gpt-35-turbo"
-                return AzureOpenAIPredictor(**kwargs)
-            case "azure-chatgpt-4":
-                kwargs["model"] = "gpt-4"
+            case "azure-openai":
                 return AzureOpenAIPredictor(**kwargs)
             case "baichuan-53b":
                 return BaiChuanPredictor(**kwargs)
@@ -199,7 +195,6 @@ class OpenAIPredictor(Predictor):
         response = await openai.ChatCompletion.acreate(
             api_key=self.token,
             api_base=self.endpoint,
-            api_type=self.api_type,
             stream=True,
             model=self.model,
             messages=[{"role": "user", "content": prompt}])
@@ -216,7 +211,6 @@ class OpenAIPredictor(Predictor):
         response = openai.ChatCompletion.create(
             api_key=self.token,
             api_base=self.endpoint,
-            api_type=self.api_type,
             stream=True,
             model=self.model,
             messages=[{"role": "user", "content": prompt}])
@@ -238,10 +232,72 @@ class OpenAIPredictor(Predictor):
             yield tokens
 
 
-class AzureOpenAIPredictor(OpenAIPredictor):
+class AzureOpenAIPredictor(Predictor):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.deployment_id = kwargs.get("deployment_id", "gpt-4")
+        self.endpoint = kwargs.get("endpoint", "https://api.openai.com/v1")
+        self.api_version = kwargs.get("api_version", "2023-05-15")
+
+        self.token = kwargs.get("token", os.environ.get("OPENAI_API_KEY", ""))
+        if not self.token:
+            raise LLMConfigError("Please specify the API token")
+        """
+        # https://github.com/openai/openai-python/issues/279
+        Example:
+        openai.proxy = {
+            "http":"http://127.0.0.1:7890",
+            "https":"http://127.0.0.1:7890"
+        }
+        """
+        proxy = json.loads(os.environ.get("OPENAI_API_PROXY", "{}"))
+        if proxy:
+            openai.proxy = proxy
         self.api_type = "azure"
+
+    async def _agenerate_stream(self, prompt):
+        response = await openai.ChatCompletion.acreate(
+            api_key=self.token,
+            api_base=self.endpoint,
+            api_type=self.api_type,
+            api_version=self.api_version,
+            stream=True,
+            deployment_id=self.deployment_id,
+            messages=[{"role": "user", "content": prompt}])
+        async for chunk in response:
+            choices = chunk["choices"]
+            if len(choices) > 0:
+                choice = choices[0]
+                if choice["finish_reason"] == "stop":
+                    return
+                content = choice["delta"]["content"]
+                yield content
+
+    def _generate_stream(self, prompt):
+        response = openai.ChatCompletion.create(
+            api_key=self.token,
+            api_base=self.endpoint,
+            api_type=self.api_type,
+            api_version=self.api_version,
+            stream=True,
+            deployment_id=self.deployment_id,
+            messages=[{"role": "user", "content": prompt}])
+        for chunk in response:
+            choices = chunk["choices"]
+            if len(choices) > 0:
+                choice = choices[0]
+                if choice["finish_reason"] == "stop":
+                    return
+                content = choice["delta"]["content"]
+                yield content
+
+    async def agenerate_stream(self, prompt):
+        async for tokens in self._agenerate_stream(prompt):
+            yield tokens
+
+    def generate_stream(self, prompt):
+        for tokens in self._generate_stream(prompt):
+            yield tokens
 
 
 class BaiChuanPredictor(Predictor):
