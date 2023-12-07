b2ebdf1f1e95fd565b272a10decc40fb6a52ee7b
Ziang Guo <ziang.gza@apecloud.com>
2023-12-07 15:21:08 +0800

    fix: wrong web router (#524)

--- kubechat/urls.py
+++ kubechat/urls.py
@@ -17,9 +17,9 @@ api.add_exception_handler(ValidationError, validation_errors)
 api.add_exception_handler(AuthenticationError, auth_errors)
 
 api.add_router("/", main_router, auth=GlobalHTTPAuth())
+api.add_router("/", web_router)
 api.add_router("/config", config_router)
 api.add_router("/feishu", feishu_router)
-api.add_router("/web", web_router)
 
 
 urlpatterns = [
