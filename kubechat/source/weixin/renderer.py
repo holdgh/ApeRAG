1d568bbb2753bec37536dcca18702a0a825b05ec
striver <44538147+lwj2015@users.noreply.github.com>
2023-12-22 17:14:28 +0800

    feat: integrate bot with wechat official account

---
+++ kubechat/source/weixin/renderer.py
@@ -0,0 +1,17 @@
+from ninja.responses import NinjaJSONEncoder
+import json
+from typing import Any, Type, Mapping
+from django.http import HttpRequest
+
+
+class MyJSONRenderer:
+    media_type = "application/json"
+    charset: str = "utf-8"
+    encoder_class: Type[json.JSONEncoder] = NinjaJSONEncoder
+
+    def render(request: HttpRequest, data: Any, *, response_status: int) -> Any:
+        encoder_class: Type[json.JSONEncoder] = NinjaJSONEncoder
+        json_dumps_params: Mapping[str, Any] = {}
+
+        # prevent Chinese characters from being converted to unicode
+        return json.dumps(data, ensure_ascii=False, cls=encoder_class, **json_dumps_params)
