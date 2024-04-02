dbb85a13eaee221b5206a72ae001a1b9619af55b
Ziang Guo <ziang.gza@apecloud.com>
2024-04-02 11:51:20 +0800

    chore: support urls in answer (#646)

--- kubechat/query/query.py
+++ kubechat/query/query.py
@@ -73,7 +73,12 @@ class QueryResult(BaseModel):
 
 
 def get_packed_answer(results, limit_length: Optional[int] = 0) -> str:
-    text_chunks = [r.text for r in results]
+    text_chunks = []
+    for r in results:
+        prefix = ""
+        if r.metadata.get("url"):
+            prefix = "The following information is from: " + r.metadata.get("url") + "\n"
+        text_chunks.append(prefix + r.text)
     answer_text = "\n\n".join(text_chunks)
     if limit_length != 0:
         return answer_text[:limit_length]
