30c6de70847eaf4a85850e6bbc2a151d3ba7aabc
earayu <earayu@163.com>
2025-04-26 11:06:02 +0800

    feat: add evaluation

--- tests/e2e_test/evaluation/eval.py
+++ tests/e2e_test/evaluation/eval.py
@@ -4,10 +4,10 @@ import pandas as pd
 from langchain_openai import ChatOpenAI
 from ragas import evaluate
 from ragas.metrics import (
-    faithfulness,  # 忠实度衡量生成答案的事实准确性，即生成的答案是否忠实于检索到的上下文内容。它主要用于评估生成阶段，避免生成模型产生“幻觉”（hallucination），即生成未被上下文支持的信息。
-    answer_relevancy,  # 答案相关性评估生成答案对输入问题的相关程度，关注答案是否直接回答问题，避免冗余或无关信息。
     context_precision,  # 上下文精确度衡量检索到的上下文是否与问题高度相关，评估检索系统是否将最相关的文档或片段排在前面。
     context_recall,  # 上下文召回率评估检索到的上下文是否涵盖了回答问题所需的所有必要信息，通常与人工标注的“地面真相”（ground truth）答案比较。
+    answer_relevancy,  # 答案相关性评估生成答案对输入问题的相关程度，关注答案是否直接回答问题，避免冗余或无关信息。
+    faithfulness,  # 忠实度衡量生成答案的事实准确性，即生成的答案是否忠实于检索到的上下文内容。它主要用于评估生成阶段，避免生成模型产生“幻觉”（hallucination），即生成未被上下文支持的信息。
     answer_correctness,  # 用LLM评估答案正确性
     answer_similarity,  # 用Embedding评估答案相似性
 )
@@ -16,6 +16,38 @@ import json
 import os
 from datetime import datetime
 from datasets import Dataset
+
+# ---------------------------------------------
+# Context Precision
+# 依赖于 question 和 context。它衡量context与question的相关性，即上下文中有多少内容是对回答问题真正有用的。
+# 适合优化检索系统的精确性，减少无关信息的干扰。例如，在法律文档检索中，避免返回无关条款。
+
+# Context Recall
+# 依赖于 question、context 和 ground truth。它衡量context是否包含了ground truth中所有必要的信息，以正确回答问题。
+# 适合确保检索系统不遗漏关键信息。例如，在医疗问答中，确保返回所有相关诊断信息。
+
+# Noise Sensitivity
+# 依赖 question、context、ground truth 和 answer
+# 用于衡量模型在处理包含噪声（无关或干扰信息）的上下文时，生成正确答案的能力。
+
+# Answer Relevance
+# 依赖于 question 和 answer
+# 用于衡量模型生成的答案与用户提问的相关性。评估模型生成的答案是否直接回答了用户的问题，而不是偏离主题或提供无关信息。
+
+# Faithfulness
+# 依赖于 context 和 answer
+# 用于衡量模型生成的答案是否忠实于提供的上下文（context），即答案是否基于上下文中的信息，而没有引入未在上下文中出现的内容（幻觉）。
+
+# Answer Correctness
+# 依赖 answer、ground truth（有时也包括 question）。
+# 使用LLM计算 answer 和 ground truth 之间的语义相似度。
+
+# Answer Semantic Similarity
+# 依赖 answer、ground truth
+# 使用embedding + 余弦相似度，计算 answer 和 ground truth 之间的语义相似度。
+
+# ---------------------------------------------
+
 
 llm_for_eval = ChatOpenAI(
     base_url=os.environ["OPENAI_API_BASE"],
