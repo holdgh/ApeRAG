2fcd3eec66ecbadee36ba3dd027e9ded1298c857
earayu <earayu@163.com>
2025-05-08 15:14:53 +0800

    feat: refactor collection config

--- frontend/src/api/models/collection-create.ts
+++ frontend/src/api/models/collection-create.ts
@@ -15,6 +15,9 @@
 
 // May contain unused imports in some cases
 // @ts-ignore
+import type { CollectionConfig } from './collection-config';
+// May contain unused imports in some cases
+// @ts-ignore
 import type { CollectionSource } from './collection-source';
 
 /**
@@ -31,10 +34,10 @@ export interface CollectionCreate {
     'title'?: string;
     /**
      * 
-     * @type {string}
+     * @type {CollectionConfig}
      * @memberof CollectionCreate
      */
-    'config'?: string;
+    'config'?: CollectionConfig;
     /**
      * 
      * @type {string}
--- frontend/src/api/models/collection.ts
+++ frontend/src/api/models/collection.ts
@@ -15,6 +15,9 @@
 
 // May contain unused imports in some cases
 // @ts-ignore
+import type { CollectionConfig } from './collection-config';
+// May contain unused imports in some cases
+// @ts-ignore
 import type { CollectionSource } from './collection-source';
 
 /**
@@ -43,10 +46,10 @@ export interface Collection {
     'description'?: string;
     /**
      * 
-     * @type {string}
+     * @type {CollectionConfig}
      * @memberof Collection
      */
-    'config'?: string;
+    'config'?: CollectionConfig;
     /**
      * 
      * @type {CollectionSource}
