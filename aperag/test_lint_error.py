68217845b1a6ae27a4c8deace25d39327275a33b
earayu <earayu@163.com>
2025-06-01 12:33:20 +0800

    Remove test file

--- aperag/test_lint_error.py
+++
@@ -1,7 +0,0 @@
-# Test file with lint errors
-
-
-def bad_function():  # bad spacing
-    x = 1 + 2  # no spaces around operators
-    print("hello world")  # f-string without placeholders
-    return x
