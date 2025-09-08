df905e5ded9e28ea16b23d6faa72646e94fd91d4
申栋栋 <sailwebs@apecloud.com>
2025-09-08 15:39:09 +0800

    fix: colelction links

--- web/src/app/workspace/collections/page.tsx
+++ web/src/app/workspace/collections/page.tsx
@@ -7,10 +7,12 @@ import {
   PageTitle,
 } from '@/components/page-container';
 
+import { Button } from '@/components/ui/button';
 import { getServerApi } from '@/lib/api/server';
 import { toJson } from '@/lib/utils';
 import { Metadata } from 'next';
 import { getTranslations } from 'next-intl/server';
+import Link from 'next/link';
 import { CollectionList } from './collection-list';
 
 export const dynamic = 'force-dynamic';
@@ -26,6 +28,7 @@ export async function generateMetadata(): Promise<Metadata> {
 export default async function Page() {
   const serverApi = await getServerApi();
   const page_collections = await getTranslations('page_collections');
+  const page_marketplace = await getTranslations('page_marketplace');
 
   let collections: CollectionView[] = [];
   try {
@@ -45,7 +48,14 @@ export default async function Page() {
         breadcrumbs={[{ title: page_collections('metadata.title') }]}
       />
       <PageContent>
-        <PageTitle>{page_collections('metadata.title')}</PageTitle>
+        <div className="flex flex-row items-center justify-between">
+          <PageTitle>{page_collections('metadata.title')}</PageTitle>
+          <Button variant="secondary" asChild>
+            <Link href="/marketplace">
+              {page_marketplace('metadata.title')}
+            </Link>
+          </Button>
+        </div>
         <PageDescription>
           {page_collections('metadata.description')}
         </PageDescription>
